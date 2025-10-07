a79dedf8ce788c6a3f0dbfe04fe2dff139f99b83
Raiane Oliveira <raiane.oliveira404@gmail.com>
2025-10-06 21:03:35 -0300

    feat: add libs, base style and main header

---
+++ js/web-components.js
@@ -0,0 +1,20 @@
+class MainHeader extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+<header class="header">
+  <div class="header__container">
+    <a href="/" class="header__logo">
+      <img src="../assets/logo_queimadas_prefeitura.png" width="176" height="54" alt="Logo Prefeitura de Queimadas, Paraíba">
+    </a>
+    <nav class="header__nav">
+      <a href="../o-que-fazer.html" class="link">O que fazer</a>
+      <a href="../restaurantes-e-pousadas.html" class="link">Restaurantes e Pousadas</a>
+      <a href="#" download class="link">Baixar mapa</a>
+    </nav>
+  </div>
+</header>
+`
+  }
+}
+
+window.customElements.define('main-header', MainHeader)
