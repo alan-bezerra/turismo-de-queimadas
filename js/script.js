715445cdfd7d66549c46bb1859f31f438a47fe78
alan-bezerra <alanbezerra_@outlook.com>
2025-10-27 08:05:44 -0300

    Pagina "o que fazer" criada, add modal.

--- js/script.js
+++ js/script.js
@@ -18,3 +18,47 @@ function toggleHeaderVisibilityOnScroll() {
     lastScrollTop = currentScrollTop
   })
 }
+
+// Modal // 
+document.addEventListener('DOMContentLoaded', () => {
+  
+  const cards = document.querySelectorAll('.o_que_fazer_cards .card')
+  const modalOverlay = document.getElementById('modal-overlay')
+  const modalClose = document.getElementById('modal-close')
+  const modalImage = document.getElementById('modal-image')
+  const modalTitle = document.getElementById('modal-title')
+  const modalText = document.getElementById('modal-text')
+
+  if (!modalOverlay || !modalClose || !modalImage || !modalTitle || !modalText) {
+    console.warn("Elementos do modal não encontrados. A funcionalidade do modal está desativada.")
+    return;
+  }
+  
+  cards.forEach(card => {
+    card.addEventListener('click', () => {
+      const title = card.dataset.title;
+      const fullText = card.dataset.fullText;
+      const imageSrc = card.dataset.imageSrc;
+
+      modalTitle.textContent = title;
+      modalText.textContent = fullText;
+      modalImage.src = imageSrc;
+      modalImage.alt = `Imagem de ${title}`
+
+      modalOverlay.classList.add('active')
+    })
+  })
+
+  // Fechar Modal
+  const closeModal = () => {
+    modalOverlay.classList.remove('active');
+  }
+
+  modalClose.addEventListener('click', closeModal);
+  modalOverlay.addEventListener('click', (event) => {
+
+    if (event.target === modalOverlay) {
+      closeModal();
+    }
+  })
+})
