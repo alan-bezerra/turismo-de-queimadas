79c9d51a94af8c4f44f92cce64bcef832fdfe136
Raiane Oliveira <raiane.oliveira404@gmail.com>
2025-11-03 09:43:55 -0300

    feat: footer

--- js/web-components.js
+++ js/web-components.js
@@ -17,6 +17,33 @@ class MainHeader extends HTMLElement {
   }
 }
 
+class Footer extends HTMLElement {
+  connectedCallback() {
+    this.innerHTML = `
+<div class="footer__container container">
+  <aside class="footer__aside">
+    <div>
+      <h2>Vai viajar? Baixe nosso mapa</h2>
+      <p>Vai viajar e precisa da versão do mapa impressa? Baixe agora clicando no botão ao lado.</p>
+    </div>
+
+    <app-button data-variant="secondary">
+      <i class="ph-fill ph-download"></i>
+      Baixar mapa
+    </app-button>
+  </aside>
+  <footer class="footer">
+    <section class="footer__content">
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at vestibulum nisi, et luctus sapien. Praesent nec turpis tortor. Sed eget dapibus felis, in pharetra ex. Nunc sed neque vitae nibh porta ornare vel vel justo. Fusce egestas, nibh id rhoncus egestas, dolor odio dapibus diam, ac ultrices nulla nulla eu nulla. Aenean euismod efficitur sem, nec cursus lorem accumsan in. Sed aliquam turpis et tellus imperdiet, at maximus nulla blandit. Nam finibus lorem risus, ut mollis lacus maximus id. 
+    </section>
+    <section class="footer__copyright">
+    </section>
+  </footer>
+</div>
+`
+  }
+}
+
 class AppButton extends HTMLElement {
   connectedCallback() {
     // Variant: primary | secondary
@@ -34,3 +61,4 @@ class AppButton extends HTMLElement {
 
 window.customElements.define('main-header', MainHeader)
 window.customElements.define('app-button', AppButton)
+window.customElements.define('main-footer', Footer)
