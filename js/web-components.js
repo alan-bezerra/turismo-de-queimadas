84b32335869be105701b52f68be2240f958130d4
Raiane Oliveira <raiane.oliveira404@gmail.com>
2025-11-26 06:46:46 -0300

    feat: footer responsiveness

--- js/web-components.js
+++ js/web-components.js
@@ -35,9 +35,56 @@ class Footer extends HTMLElement {
   </aside>
   <footer class="footer">
     <section class="footer__content">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at vestibulum nisi, et luctus sapien. Praesent nec turpis tortor. Sed eget dapibus felis, in pharetra ex. Nunc sed neque vitae nibh porta ornare vel vel justo. Fusce egestas, nibh id rhoncus egestas, dolor odio dapibus diam, ac ultrices nulla nulla eu nulla. Aenean euismod efficitur sem, nec cursus lorem accumsan in. Sed aliquam turpis et tellus imperdiet, at maximus nulla blandit. Nam finibus lorem risus, ut mollis lacus maximus id. 
+      <div class="footer__social">
+        <img src="../assets/logo_queimadas_branca.png" width="176" height="54" alt="Logo Prefeitura de Queimadas, Paraíba">
+          <p>
+        Explore nossas formações rochosas únicas, desfrute da gastronomia local e viva experiências inesquecíveis no coração da Paraíba.</p>
+          <div>
+            <a href="#" target="_blank">
+              <i class="ph-facebook-logo ph-fill"></i>
+            </a>
+            <a href="#" target="_blank">
+              <i class="ph-instagram-logo ph-fill"></i>
+            </a>
+            <a href="#" target="_blank">
+              <i class="ph-whatsapp-logo ph-fill"></i>
+            </a>
+            <a href="https://queimadas.pb.gov.br" target="_blank">
+              <i class="ph-globe ph-fill"></i>
+            </a>
+          </div>
+      </div>
+      <div class="footer__links">
+        <h5>Pontos Turísticos</h5>
+        <ul>
+          <li><a href="o-que-fazer.html" class="link">Pedra da Caveira</a></li>
+          <li><a href="o-que-fazer.html" class="link">Pedra do Touro</a></li>
+          <li><a href="o-que-fazer.html" class="link">Serra do Bodopitá</a></li>
+          <li><a href="o-que-fazer.html" class="link">Centro Cultural</a></li>
+        </ul>
+      </div>
+      <div class="footer__links">
+        <h5>Gastronomia</h5>
+        <ul>
+          <li><a href="restaurantes-e-pousadas.html" class="link">Restaurantes</a></li>
+          <li><a href="restaurantes-e-pousadas.html" class="link">Pousadas</a></li>
+          <li><a href="restaurantes-e-pousadas.html" class="link">Comida Típica</a></li>
+          <li><a href="restaurantes-e-pousadas.html" class="link">Tapiocas</a></li>
+        </ul>
+      </div>
+      <div class="footer__links">
+        <h5>Informações</h5>
+        <ul>
+          <li><a href="quiz.html" class="link">Quiz das Pedras</a></li>
+          <li><a href="#" class="link">Mapa Turístico</a></li>
+          <li><a href="#" class="link">Como Chegar</a></li>
+          <li><a href="#" class="link">Contato</a></li>
+          <li><a href="https://queimadas.pb.gov.br" target="_blank" class="link">Site Oficial</a></li>
+        </ul>
+      </div>
     </section>
     <section class="footer__copyright">
+      <p>© ${new Date().getFullYear()} Prefeitura de Queimadas. Desenvolvido por <a href="https://unifacisa.edu.br" target="_blank">Unifacisa</a>.</p>
     </section>
   </footer>
 </div>
